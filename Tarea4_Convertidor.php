ed342a37dfa3f5b7d9a58e672332522824471e43
carloslopez1890 <carloslopez1890@hotmail.com>
2013-09-30 16:57:47 +0200

    Covertidor

---
+++ Tarea4_Convertidor.php
@@ -0,0 +1,24 @@
+
+
+<?php 
+
+$ casa = 15;
+$ dolar = 14;
+
+
+
+echo "Dolar a convertir es  ".$dolar;
+echo "valor de peso hoy es :  ".$casa;
+
+$ convrtir = dolar * casa;
+
+echo "la cantidad en pesos es :  ".$convrtir;
+
+
+
+
+
+
+
+
+ ?>
